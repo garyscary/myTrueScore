d7e062abacf9d71862a61ffea0559173216f17da
Rupehra Chouhan <rupehra1@gmail.com>
2018-01-27 18:32:13 -0700

    ADD: Models

--- mysite/mytruescore/models.py
+++ mysite/mytruescore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class AllUserID(models.Model):
+    username = models.CharField(max_length=100)
+    website = models.CharField(max_length=200)
+    
+class User(models.Model):
+    usernames = models.ManyToManyField('AllUserID')
+    name = models.CharField(max_length=100)
+    id = models.CharField(max_length=100,primary_key=True,unique=True)
+    
